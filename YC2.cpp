9677da004bf1d4e67943ffce3dbc35529c26dbe9
tannguyen2k5 <147480969+tannguyen2k5@users.noreply.github.com>
2023-12-06 13:56:45 +0700

    Add files via upload

---
+++ YC2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{ 
+	printf("************************************************\n");
+	printf("***     1.Nhap chuoi S, chuan hoa chuoi S    ***\n");
+	printf("***     2.Nhap mot ki tu, dem trong chuoi S  ***\n");
+	printf("***     0.Thoat                              ***\n");
+	printf("************************************************\n");
+	
+}
